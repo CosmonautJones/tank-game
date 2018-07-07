47bfddf4f9ac1299f4bd460c267bfac8c226ad26
Travis Jones <travisj_jones@hotmail.com>
2018-07-07 15:30:32 -0500

    got mouse positioning with proper angle arguments for further calculations

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btnStart = document.querySelector('.btnStart');
 const gameOverEle = document.getElementById('gameOverEle');
 const container = document.getElementById('container');
+const box = document.querySelector('.box');
+const boxCenter = [box.offsetLeft + box.offsetWidth / 2, box.offsetTop + (box.offsetHeight / 2)];
 
 let gamePlay = false;
 let player;
@@ -8,6 +10,18 @@ let animateGame;
 
 btnStart.addEventListener('click', startGame);
 container.addEventListener('mousedown', mouseDown);
+container.addEventListener('mousemove', mousePosition);
+
+function mousePosition(event) {
+  // console.log(event);
+  let mouseAngle = getDeg(event);
+  console.log(mouseAngle);
+}
+
+function getDeg(event) {
+  let angle = Math.atan2(event.clientX - boxCenter[0], (event.clientY - boxCenter[1]));
+  return angle * (180 / Math.PI);
+}
 
 function mouseDown(event) {
   if (gamePlay) {
